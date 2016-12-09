9d116bea1876f05c2d9c4bdc27b4813dddb94cd4
rihuiwangxun <sz_lemeitong@163.com>
2016-12-09 16:39:55 +0800

    修改保存列表记录接口，由原来的保存到路由表，更换到数据库当中

--- host/sdcard/MusicListDb.c
+++ host/sdcard/MusicListDb.c
@@ -18,12 +18,13 @@ typedef struct{
 
 MusicList_t *Mlist=NULL;
 
+//数据库表:保存歌曲ID号和名字 (用来播放列表用)
 static void CreateMusicTable(const char *tableMusic){
 	char sql[128]={0};
 	snprintf(sql,128,"create table if not exists %s (id INTEGER PRIMARY KEY,name VARCHAR(64) not null unique);",tableMusic);            
 	CreateTable(sql);
 }
-
+//保存歌曲列表信息,缓存播放历史记录值
 static void CreateMusicListMesage(const char *tableMusic){
 	char sql[128]={0};
 	snprintf(sql,128,"create table if not exists %s (name VARCHAR(64) not null unique,number INTEGER,DirTime INTEGER);",tableMusic);            
@@ -39,6 +40,7 @@ static void checkpath(const char *path,char *getPath){
 		strncat(getPath,"/",1);
 	}
 }
+//根据目录，获取表名，去掉后面的 / 符号
 static void GetTableName(const char *path,char *destPath){
 	int len = strlen(path);
 	if(*(path+len-1)=='/'){
@@ -91,6 +93,7 @@ static enum{
 };
 
 void SaveSystemPlayNum(void){
+#if 0	
 	set_paly_num(Mlist->list[0].playindex,mp3_N);
 	usleep(100);
 	set_paly_num(Mlist->list[1].playindex,story_N);
@@ -99,9 +102,24 @@ void SaveSystemPlayNum(void){
 	usleep(100);
 	set_paly_num(Mlist->list[3].playindex,guoxue_N);
 	usleep(100);
+#else
+	List_t cachelist;
+	int i=0;
+	for(i=0;i<4;i++){
+		snprintf(cachelist.listname,24,"%s%s","cache",Mlist->list[i].listname);
+		if(InsertMusicMessageSQL(MESSAGE_TABLE,cachelist.listname,Mlist->list[i].playindex,0)){
+			UpdateSqlByMessage(MESSAGE_TABLE,cachelist.listname,Mlist->list[i].playindex,0);
+		}
+		printf("Mlist->list[%d].playindex =%d\n",i,Mlist->list[i].playindex);
+	}
+#endif
 }
+/*
+开机读取目录文件，加载到数据库当中
+*/
 int SysOnloadMusicList(const char *sdcard,const char *mp3Music,const char *story,const char *english,const char *guoxue){
 	char sqlPath[128]={0};
+	
 	checkpath(sdcard,sqlPath);
 	strcat(sqlPath,DATABASE_NAME);
 	if(OpenSql(sqlPath) != 0)
@@ -109,13 +127,26 @@ int SysOnloadMusicList(const char *sdcard,const char *mp3Music,const char *story
 
 	int i=0,listNum=MUSIC_LIST;
 	GetTableName(mp3Music,Mlist->list[0].listname);
-	get_paly_num(&Mlist->list[0].playindex,mp3_N);
 	GetTableName(story,Mlist->list[1].listname);
-	get_paly_num(&Mlist->list[1].playindex,story_N);
 	GetTableName(english,Mlist->list[2].listname);
-	get_paly_num(&Mlist->list[2].playindex,english_N);
 	GetTableName(guoxue,Mlist->list[3].listname);
+#if 0	
+	get_paly_num(&Mlist->list[0].playindex,mp3_N);
+	get_paly_num(&Mlist->list[1].playindex,story_N);
+	get_paly_num(&Mlist->list[2].playindex,english_N);
 	get_paly_num(&Mlist->list[3].playindex,guoxue_N);
+#else
+	List_t cachelist;
+	for(i=0;i<4;i++){
+		// 缓存目录当中的 cachemp3 字段里面，Nums记录的是关机之后，播放的num标号
+		memset(&cachelist,0,sizeof(List_t));
+		snprintf(cachelist.listname,24,"%s%s","cache",Mlist->list[i].listname);
+		GetMusicMessageSQL(MESSAGE_TABLE,&cachelist);
+		Mlist->list[i].playindex=cachelist.Nums;
+		printf("Mlist->list[%d].playindex =%d\n",i,Mlist->list[i].playindex);
+	}
+#endif
+
 #ifdef DOWN_URL_MUSIC	
 	CreateMusicTable(XIMALA_MUSIC);
 	sprintf(Mlist->list[MUSIC_LIST-1].listname,"%s",XIMALA_MUSIC);
@@ -124,7 +155,7 @@ int SysOnloadMusicList(const char *sdcard,const char *mp3Music,const char *story
 	listNum-=1;
 #endif	
 	CreateMusicListMesage(MESSAGE_TABLE);
-	for(;i<listNum;i++){
+	for(i=0;i<listNum;i++){
 		GetMusicMessageSQL(MESSAGE_TABLE,&(Mlist->list[i]));
 		CreateMusicTable(Mlist->list[i].listname);
 		//printf("Mlist->list[0].listname =%s\n",Mlist->list[i].listname);
@@ -217,7 +248,7 @@ int checkMusicDb(const char *sdcard,const char *musicDir){
 void testPlayList(void){
 	char musicname[128]={0};
 	int i=0;
-	for(i=0;i<12;i++)	{
+	for(i=0;i<7;i++)	{
 		//GetSdcardMusic((const char *)"sdcard",(const char *)"keji",musicname,PLAY_NEXT);
 		GetSdcardMusic((const char *)"sdcard/",(const char *)"why/",musicname,PLAY_PREV);
 		printf("musicname[%d] = %s\n",i,musicname);
@@ -235,11 +266,12 @@ void test_GetlocalSdcard(void){
 }
 int main(void){
 	InitMusicList();
-	SysOnloadMusicList((const char *)"sdcard/",(const char *)"keji/",(const char *)"why/",(const char *)"english/");
-	//testPlayList();
+	SysOnloadMusicList((const char *)"sdcard/",(const char *)"keji/",(const char *)"why/",(const char *)"english/",(const char *)"guoxue/");
+	testPlayList();
 	InsertXimalayaMusic((const char *)XIMALA_MUSIC,(const char *)"testmusic11");
 	InsertXimalayaMusic((const char *)XIMALA_MUSIC,(const char *)"testmusic12");
 	InsertXimalayaMusic((const char *)XIMALA_MUSIC,(const char *)"testmusic13");
+	SaveSystemPlayNum();
 	test_GetlocalSdcard();
 	CleanMusicList();
 	return 0;
